8d15827927eccc2d754a47d698dfcc25777932aa
Emil Renner Berthing <esmil@mailme.dk>
2012-02-20 16:26:38 +0100

    power: updated to use lem-http HTTP parser

--- plugins/power.lua
+++ plugins/power.lua
@@ -1,21 +1,26 @@
 local utils   = require 'lem.utils'
 local streams = require 'lem.streams'
-local format  = string.format
+require 'lem.http'
 
-local function get_power(cont)
-	local conn, err = streams.tcp_connect('space.labitat.dk', 8080)
-	if not conn then return cont(err) end
+local format, tonumber  = string.format, tonumber
 
-	local ok, err = conn:write('GET /last HTTP/1.1\r\nHost: space.labitat.dk\r\nConnection: close\r\n\r\n')
-	if not ok then return cont(err) end
+local function get_power()
+	local iconn, oconn = streams.tcp_connect('space.labitat.dk', 8080)
+	if not iconn then return oconn end
 
-	local res, err = conn:read('*a')
-	if not res then return cont(err) end
+	local ok, err = oconn:write('GET /last HTTP/1.1\r\nHost: space.labitat.dk\r\nConnection: close\r\n\r\n')
+	if not ok then return err end
 
-	local ms = res:match('\r\n\r\n%[%d+,(%d+)%]')
-	if not ms then cont('received unexpected answer from server') end
+	local res, err = iconn:read('HTTPResponse')
+	if not res then return err end
 
-	return cont(format('%dW', 3600000 / tonumber(ms)))
+	local body, err = res:body()
+	if not body then return err end
+
+	local ms = body:match('%[%d+,(%d+)%]')
+	if not ms then return 'received unexpected answer from server' end
+
+	return format('%dW', 3600000 / tonumber(ms))
 end
 
 PRIVMSG(function(msg)
@@ -23,14 +28,14 @@ PRIVMSG(function(msg)
 	if not msg[2]:match('[Ss][Tt][Rr]..?[Mm]') then return end
 
 	if msg[1] == config.nick then
-		return utils.spawn(get_power, function(reply)
-			send('PRIVMSG %s :%s\r\n', msg.nick, reply)
+		return utils.spawn(function()
+			send('PRIVMSG %s :%s\r\n', msg.nick, get_power())
 		end)
 	end
 
 	if msg[2]:match(format('^%s[:, ]', config.nick)) then
-		return utils.spawn(get_power, function(reply)
-			send('PRIVMSG %s :%s: %s\r\n', msg[1], msg.nick, reply)
+		return utils.spawn(function()
+			send('PRIVMSG %s :%s: %s\r\n', msg[1], msg.nick, get_power())
 		end)
 	end
 end)
